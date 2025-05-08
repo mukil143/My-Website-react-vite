08571709348d08e99bac18c9422217fa296f0d30
mukilan143 <mukilanmukilan174@gmail.com>
2025-05-08 18:22:19 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Article from "./components/Article";
 import Tablesec from "./components/Tablesec";
